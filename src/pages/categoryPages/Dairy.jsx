b56178598f9e995fe1f35d2289b77750c2aef5b0
dore96 <doredelman@gmail.com>
2023-07-06 03:04:27 +0300

    init category navbar

--- src/pages/categoryPages/Dairy.jsx
+++ src/pages/categoryPages/Dairy.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-import {Box, Typography} from "@mui/material";
+import React, {useState} from "react";
+import {Box} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import ProductCard from "../../components/Product";
 import ShopNavbar from "../../components/ShopNavbar";
+import PopupMessage from "../../components/PopupMessage";
 const Dairy = ({products,handleAddToCart}) => {
+    const [popupMessage, setPopupMessage] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleAddToCartFromShop = (product, quantity) => {
+        // Update the popup state variables
+        setShowPopup(true);
+        setPopupMessage(`Added ${quantity} ${product.unit} of ${product.name} to the cart`);
+
+        // Reset the popup state variables after a certain duration
+        setTimeout(() => {
+            setShowPopup(false);
+            setPopupMessage('');
+        }, 2000); // 2 seconds
+
+        handleAddToCart(product, quantity);
+    };
+
+    const closePopup = () => {
+        setShowPopup(false);
+        setPopupMessage('');
+    };
     return (
         <div>
             <ShopNavbar/>
@@ -19,12 +41,16 @@ const Dairy = ({products,handleAddToCart}) => {
                     <Grid item xs={12} sm={6} md={1.5} key={index}>
                         <ProductCard
                             product={product}
-                            handleAddToCart ={handleAddToCart}
+                            handleAddToCart ={handleAddToCartFromShop}
                         />
                     </Grid>
                 ))}
             </Grid>
         </Box>
+            {/* Popup message */}
+            {showPopup && (
+                <PopupMessage message={popupMessage} duration={2000} onClose={closePopup} />
+            )}
             </div>
     );
 };
